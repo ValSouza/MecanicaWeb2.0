7c9ced312fcbf15206140e92ff2a7a03f95dc6e7
ValSouza <valdecir_souza@outlook.com>
2021-04-07 19:00:38 -0300

    Cliente e funcionario VO

---
+++ vo/ClientesVO.php
@@ -0,0 +1,54 @@
+<?php
+
+    class ClientesVO{
+
+        private $idCliente;
+        private $nomeCliente;
+        private $phoneCliente;
+        private $addressCliente;
+        private $idUsuario;
+
+        public function setIdCliente($id){
+            $this->idCliente = $id;
+        }
+
+        public function getIdCliente(){
+            return $this->idCliente;
+        }
+
+        public function setNomeCliente($nome){
+            $this->nomeCliente = ltrim(trim($nome));
+        }
+
+        public function getNomeCliente(){
+            return $this->nomeCliente;
+        }
+
+        public function setPhoneCliente($phone){
+            $this->phoneCliente = ltrim(trim($phone));
+        }
+        
+        public function getPhoneCliente(){
+            return $this->phoneCliente;
+        }
+
+        public function setAddressCliente($address){
+            $this->addressCliente = ltrim(trim($address));
+        }
+
+        public function getAddressCliente(){
+            return $this->addressCliente;
+        }
+
+        public function setIdUsuario($usuario){
+            $this->idUsuario = $usuario;
+        }
+
+        public function getIdUsuario(){
+            return $this->idUsuario;
+        }
+        
+    }
+
+
+?>
---
+++ vo/FuncionarioVO.php
@@ -0,0 +1,62 @@
+<?php 
+
+    class FuncionarioVO{
+
+        private $idStaff;
+        private $nomeStaff;
+        private $phoneStaff;
+        private $addressStaff;
+        private $situation;
+        private $idUsuario;
+
+        public function setIdStaff($id){
+            $this->idStaff = $id;
+        }
+
+        public function getIdStaff(){
+            return $this->idStaff;
+        }
+
+        public function setNomeStaff($nome){
+            $this->nomeStaff = ltrim(trim($nome));
+        }
+
+        public function getNomeStaff(){
+            return $this->nomeStaff;
+        }
+
+        public function setPhoneStaff($phone){
+            $this->phoneStaff = ltrim(trim($phone));
+        }
+
+        public function getPhoneStaff(){
+            return $this->phoneStaff;
+        }
+
+        public function setSddressStaff($address){
+            $this->addressStaff = ltrim(trim($address));
+        }
+
+        public function getAddressStaff(){
+            return $this->addressStaff;
+        }
+
+        public function setSituation($sit){
+            $this->situation = $sit;
+        }
+
+        public function getSituation(){
+            return $this->situation;
+        }
+
+        public function setIdUsuario($idU){
+            $this->idUsuario = $idU;
+        }
+
+        public function getIdUsuario(){
+            return $this->idUsuario;
+        }
+    }
+
+
+?>
